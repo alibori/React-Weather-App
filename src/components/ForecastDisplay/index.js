3450ce92fdc984d0de73a05f35b999038226ae66
Axel Libori Roch <a.libori@gmail.com>
2021-01-24 15:53:53 +0100

    date calculator

--- src/components/ForecastDisplay/index.js
+++ src/components/ForecastDisplay/index.js
@@ -6,11 +6,12 @@ import './styles.css'
 export default function ForecastDisplay(props){
     const daily = props.daily
     const date = new Date()
+    const oneDay = 24*60*60*1000
 
     return (
         <ul>
             {daily.map((day, index) =>
-                <li key={day.dt}>{date.getDate()+index + '/' + (date.getMonth()+1)}: <img alt="weather icon" src={API_IMG_URL+day.weather[0].icon+".png"} /> <span className="temp temp-min">{day.temp.min}ºC</span>-<span className="temp temp-max">{day.temp.max}ºC</span></li>
+                <li key={day.dt}>{(new Date(date + (index*oneDay))).getDate()+index + '/' + ((new Date(date + (index*oneDay))).getMonth()+1)}: <img alt="weather icon" src={API_IMG_URL+day.weather[0].icon+".png"} /> <span className="temp temp-min">{day.temp.min}ºC</span>-<span className="temp temp-max">{day.temp.max}ºC</span></li>
             )}
         </ul>
     )
--- src/services/settings.js
+++ src/services/settings.js
@@ -1,4 +1,4 @@
-export const API_KEY = 'PASTE YOUR API KEY HERE'
+export const API_KEY = 'PASTE YOU API KEY HERE'
 
 export const API_URL = 'http://api.openweathermap.org/data/2.5'
 
